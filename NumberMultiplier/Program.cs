cd93bb5ba1c0b504a0cfa9cfd56226bfcd85b976
Andy Million <amillion3@gmail.com>
2018-08-25 10:00:41 -0500

    added user selection interface

--- NumberMultiplier/Program.cs
+++ NumberMultiplier/Program.cs
@@ -6,18 +6,36 @@ namespace NumberMultiplier
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please input a comma-separated string of numbers,");
-            Console.WriteLine("such as '1, 2, 3, 4, 5' :");
-            string inputString = Console.ReadLine();
-
-            int total = 1;
-            string [] inputArray = inputString.Split(',');
-            foreach (string a in inputArray)
+            Console.WriteLine("Please choose an option below:");
+            Console.WriteLine("1. Comma-separated string of numbers to MULTIPLY together.");
+            Console.WriteLine("2. Comma-separated string of numbers to SQUARE individually.");
+            string userSelection = Console.ReadLine();
+            if (userSelection == "1")
             {
-                total = int.Parse(a) * total;
+                Console.WriteLine("Please input a comma-separated string of numbers,");
+                Console.WriteLine("such as '1, 2, 3, 4, 5' :");
+                string inputString = Console.ReadLine();
+
+                int total = 1;
+                string [] inputArray = inputString.Split(',');
+                foreach (string a in inputArray)
+                {
+                    total = int.Parse(a) * total;
+                }
+                Console.WriteLine("The final total is " + total);
+                Console.ReadLine();
+
             }
-            Console.WriteLine("The final total is " + total);
-            Console.ReadLine();
+            else if (userSelection == "2")
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("You failed.");
+            }
+
+
         }
     }
 }
