a894d3d3e436f62ca9a32b51ea0e06de5350ac34
Andy Million <amillion3@gmail.com>
2018-08-25 09:55:12 -0500

    part 1 complete

--- NumberMultiplier/Program.cs
+++ NumberMultiplier/Program.cs
@@ -6,7 +6,8 @@ namespace NumberMultiplier
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please input a comma-separated string of numbers:");
+            Console.WriteLine("Please input a comma-separated string of numbers,");
+            Console.WriteLine("such as '1, 2, 3, 4, 5' :");
             string inputString = Console.ReadLine();
 
             int total = 1;
@@ -15,6 +16,8 @@ namespace NumberMultiplier
             {
                 total = int.Parse(a) * total;
             }
+            Console.WriteLine("The final total is " + total);
+            Console.ReadLine();
         }
     }
 }
